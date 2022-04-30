34e5d2d788f4677f6f16dd199063bbe721cb61de
tgiqfe <tqji31001@gmail.com>
2022-05-01 00:35:10 +0900

    Add nic class.

---
+++ EnumRun/Log/NIC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumRun.Log
+{
+    internal class NIC
+    {
+        public string Name { get; set; }
+        public string[] IPv4Addresses { get; set; }
+        public string[] IPv4SubnetMasks { get; set; }
+        public string[] IPv4DefaultGateways { get; set; }
+        public string[] IPv4DNSServers { get; set; }
+
+        //  ここにネットワークインタフェース情報を
+    }
+}
--- EnumRun/Log/SystemInfoLog.cs
+++ EnumRun/Log/SystemInfoLog.cs
@@ -1,12 +1,16 @@
-﻿using System.Management;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
 using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text;
 
 namespace EnumRun.Log
 {
-    internal class LogBody
+    internal class SystemInfoLog
     {
         #region Serial parameter
 
@@ -35,38 +39,36 @@ namespace EnumRun.Log
 
         #endregion
 
-        public string Tag { get { return "EnumRun_Log"; } }
+        public string Tag { get { return "EnumRun_SystemInfo"; } }
         public string Date { get; set; }
-        public LogLevel Level { get; set; }
-        public string ProcessName { get; set; }
-        public string ScriptFile { get; set; }
-        public string UserName { get; set; }
         public string HostName { get; set; }
-        public string Message { get; set; }
+        public string OS { get; set; }
+        public string OSVersion { get; set; }
+        public string AppFilePath { get; set; }
+        public string AppVersion { get; set; }
+        public NIC[] NetworkInterfaces { get; set; }
+        public DateTime BootupTime { get; set; }
+        public DateTime LogonTime { get; set; }
 
         private JsonSerializerOptions _options = new JsonSerializerOptions()
         {
             Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            //WriteIndented = true,
+            WriteIndented = true,
             Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
         };
 
         public void Init()
         {
-            this.ProcessName = Item.ProcessName;
-            this.UserName = Environment.UserName;
             this.HostName = Environment.MachineName;
+
+            ManagementObject mo = new ManagementClass("Win32_OperatingSystem").
+                GetInstances().
+                OfType<ManagementObject>().
+                First();
+            this.OS = mo["Caption"] as string;
+            this.OSVersion = mo["Version"] as string;
+            this.AppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
 
-        public string GetLog(LogLevel level, string scriptFile, string message)
-        {
-            this.Date = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
-            this.Level = level;
-            this.ScriptFile = scriptFile;
-            this.Message = message;
-            this._index++;
-
-            return JsonSerializer.Serialize(this, _options);
-        }
     }
 }
